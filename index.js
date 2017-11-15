2e39ab8e01190b580329f2fdafae59f1166c3393
Hoàng Tâm <phanhoangtam.pht@gmail.com>
2017-11-15 20:45:17 +0700

    Cài đặt react-native-youtube

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Player from './Player.js';
 
-AppRegistry.registerComponent('eTicket', () => App);
+AppRegistry.registerComponent('eTicket', () => Player);
